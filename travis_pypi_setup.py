931a856cc8723bdcfadf9b66cb8f7565232a3838
Albert Kottke <albert.kottke@gmail.com>
2016-03-09 16:55:43 -0800

    Fixed exception.

--- travis_pypi_setup.py
+++ travis_pypi_setup.py
@@ -14,10 +14,9 @@ from cryptography.hazmat.primitives.serialization import load_pem_public_key
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives.asymmetric.padding import PKCS1v15
 
-
 try:
     from urllib import urlopen
-except:
+except ImportError:
     from urllib.request import urlopen
 
 
