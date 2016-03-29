b97c1cbe85522bc7e546c99ec99ed902163cb000
Nasser Marafi <nasser.marafi@gmail.com>
2016-03-28 22:48:36 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setup(
         'pygmm'
     ],
     package_dir={'pygmm':
-                 'pygymm'},
+                 'pygmm'},
     package_data={'pygmm': ['data/*']},
     include_package_data=True,
     install_requires=requirements,
